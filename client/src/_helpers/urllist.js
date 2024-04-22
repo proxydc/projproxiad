4267a079753345a2a71f558200980d1d4486e24f
proxydc <alexproxiad@gmail.com>
2024-04-22 10:14:39 +0200

    testing1 correction1

--- client/src/_helpers/urllist.js
+++ client/src/_helpers/urllist.js
@@ -1,7 +1,7 @@
 const backEndURL =
-    process.env.NODE_ENV == "production"
-        ? process.env.VUE_APP_DBPRODURL
-        : process.env.VUE_APP_DBDEVURL;
+    process.env.NODE_ENV == "production" ?
+    process.env.VUE_APP_DBPRODURL :
+    process.env.VUE_APP_DBDEVURL;
 const baseURL = backEndURL + "/api/v1/database/";
 let acc = localStorage.getItem("useraccount");
 class urllist {
@@ -18,21 +18,13 @@ class urllist {
         return baseURL + "account/" + id;
     }
     static getDcsUrl() {
-        if(acc==null)
-        {
-            acc = localStorage.getItem("useraccount");
-        }
-        if (acc == "admin") { return baseURL + "dc"; }
-        else {
+        acc = localStorage.getItem("useraccount");
+        if (acc == "admin") { return baseURL + "dc"; } else {
             return baseURL + "dcByManager/" + parseInt(localStorage.getItem("manager_id"));
         }
     }
     static getDcUrl(id) {
-        if(acc==null)
-        {
-            acc = localStorage.getItem("useraccount");
-        }
-        //const acc= localStorage.getItem("useraccount");
+        acc = localStorage.getItem("useraccount");
         if (acc == "user" || acc == "admin")
             return baseURL + "dc/" + id;
         else
@@ -48,19 +40,19 @@ class urllist {
         return baseURL + "dc/add";
     }
     static getDelDcUrl(id) {
-        return baseURL + "dc/" + id;
-    }
-    /*static update(id, data, isCompleted) {
-        return baseurl.put(`/dc/${id}?completed=${isCompleted}`, data);
-    }
+            return baseURL + "dc/" + id;
+        }
+        /*static update(id, data, isCompleted) {
+            return baseurl.put(`/dc/${id}?completed=${isCompleted}`, data);
+        }
 
-    static searchByName(name) {
-        return baseurl.get(`/dc?name=${name}`);
-    }
+        static searchByName(name) {
+            return baseurl.get(`/dc?name=${name}`);
+        }
 
-    static searchByTag(tag) {
-        return baseurl.get(`/dc?tag=${tag}`);
-    }*/
+        static searchByTag(tag) {
+            return baseurl.get(`/dc?tag=${tag}`);
+        }*/
 }
 
 export default urllist;
