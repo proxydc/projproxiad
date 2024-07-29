1e19b51149e3cd8f1e81d8f6b5df01ce04ae4980
proxydc <alexproxiad@gmail.com>
2024-07-29 14:43:18 +0200

    testing1 correction8-tri par date

--- server/src/database/queries.js
+++ server/src/database/queries.js
@@ -8,8 +8,8 @@ const deleteAccountById = "delete from account where id = $1";
 const getAuthentification = "select * from account a where a.login_name = $1 and a.pass_word = $2";
 
 //queries reg DCS
-const getDCs = "select d.*, ds.label as status_name, acc.display_name as manager_name from dc d, dc_status ds, account acc where d.dc_status = ds.id and d.manager_id = acc.id";
-const getDCsByManagerID = "select d.*, ds.label as status_name, acc.display_name as manager_name from dc d, dc_status ds, account acc where d.dc_status = ds.id and d.manager_id = acc.id and d.ref_managers LIKE $1";
+const getDCs = "select d.*, ds.label as status_name, acc.display_name as manager_name from dc d, dc_status ds, account acc where d.dc_status = ds.id and d.manager_id = acc.id order by d.creation_date desc";
+const getDCsByManagerID = "select d.*, ds.label as status_name, acc.display_name as manager_name from dc d, dc_status ds, account acc where d.dc_status = ds.id and d.manager_id = acc.id and d.ref_managers LIKE $1 order by d.creation_date desc";
 const getDCById = "select d.*, acc.display_name as manager_name, ds.label as status_name from dc d, dc_status ds, account acc where d.id = $1 and d.dc_status = ds.id and d.manager_id = acc.id ";
 const getDCByIdCandidat = "select d.*, ds.label as status_name from dc d, dc_status ds where d.id = $1 and d.dc_status = ds.id";
 const getDCDocById = "select dc.document, dc.familyname, dc.firstname, dc.nbexps, dc.poste from dc where id = $1 limit 1";
@@ -18,8 +18,8 @@ const addDC = "insert into dc(familyname, firstname, email, dc_status, document,
 const updateDCByAdmin = "update dc set familyname=$2, firstname=$3, email=$4, dc_status=$5, tags = $6, nbexps = $7, poste = $8, modification_date = $9, ref_managers = $10 where id = $1"; //controle sur email
 const updateDCDoc = "update dc set document = $2, dc_status=$3 where id = $1";
 const deleteDCById = "delete from dc where id = $1";
-const getDCsParDate = "select d.*, ds.label as status_name, acc.display_name as manager_name from dc d, dc_status ds, account acc where d.dc_status = ds.id and d.manager_id = acc.id and d.creation_date >= $1 and  d.creation_date <= $2";
-const getDCsByManagerIDParDate = "select d.*, ds.label as status_name, acc.display_name as manager_name from dc d, dc_status ds, account acc where d.dc_status = ds.id and d.manager_id = acc.id and d.ref_managers LIKE $1 and d.creation_date >= $2 and  d.creation_date <= $3";
+const getDCsParDate = "select d.*, ds.label as status_name, acc.display_name as manager_name from dc d, dc_status ds, account acc where d.dc_status = ds.id and d.manager_id = acc.id and d.creation_date >= $1 and  d.creation_date <= $2 order by d.creation_date desc";
+const getDCsByManagerIDParDate = "select d.*, ds.label as status_name, acc.display_name as manager_name from dc d, dc_status ds, account acc where d.dc_status = ds.id and d.manager_id = acc.id and d.ref_managers LIKE $1 and d.creation_date >= $2 and  d.creation_date <= $3 order by d.creation_date desc";
 
 
 //queries reg DC-Status
